90482422dec0f1c86af3509a342d1d4b5821c3c4
huadong <hd2260332898@gmail.com>
2018-10-22 14:42:40 +0800

    first commit

---
+++ src/main/java/com/ys/websocket/WebsocketApplication.java
@@ -0,0 +1,16 @@
+package com.ys.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author HD
+ * @date 2018/10/22 14:40
+ */
+@SpringBootApplication
+public class WebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketApplication.class, args);
+    }
+}
